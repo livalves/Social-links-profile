8ecbfef44745319e7d670183c513d167f2d73485
livalves <liviamabelle@gmail.com>
2024-02-24 22:57:50 -0300

    Estilização da página

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Social links profile
 
-- Página centralizada para apresentar perfis em diferentes redes sociais utilizando React.
+- Página centralizada para apresentar perfis em diferentes redes sociais.
 - Desafio Vai na Web | Curso de Desenvolvimento Front-end 
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
+
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className='container'>
+            <img className='photo' src="https://i.pinimg.com/736x/fd/6c/cd/fd6ccdb97d1857a470f748a280ccfd46.jpg"/>
+            <h1>
+                Lívia Alves
+                <span className='locality'>Barbalha, Ceará</span>
+            </h1>
+
+            <div className='profession'>
+                <span >"Desenvolvedora Front-end"</span>
+            </div>
+
+            <div className='container-buttons'>
+                <button>GitHub</button>
+                <button>Gmail</button>
+                <button>Linkedin</button>
+                <button>Instagram</button>
+            </div>
+        </div>
+      </main>
     </>
   )
 }
